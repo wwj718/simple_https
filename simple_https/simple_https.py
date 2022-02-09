55e55c3b13247a9fc1676077deef32412bc00995
wwj718 <wuwenjie718@gmail.com>
2022-02-09 11:53:22 +0800

    support mkcert

--- simple_https/simple_https.py
+++ simple_https/simple_https.py
@@ -6,30 +6,31 @@ import http.server, ssl
 
 base_path = os.path.dirname(os.path.realpath(__file__))
 src_path = os.path.join(base_path, "src")
-server_pem = os.path.join(src_path, "server.pem")
+# server_pem = os.path.join(src_path, "server.pem")
 
 def main():
+    '''
+    work with mkcert: https://github.com/FiloSottile/mkcert
+        mkcert -install
+        mkcert example.com "*.example.com" example.test localhost 127.0.0.1 ::1
+    simple-https -k ~/example.com+5-key.pem -c ~/example.com+5.pem
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", dest="certfile", default="None", help="certfile")
+    parser.add_argument("-k", dest="keyfile", default="None", help="keyfile")
     parser.add_argument("-p", dest="port", default=8443, help="http server port")
     parser.add_argument("-a", dest="address", default="0.0.0.0", help="address")
     parser.add_argument("-d", dest="directory", default=".", help="directory")
     args = parser.parse_args()
 
-    if args.certfile != 'None':
-        certfile = args.certfile
-    else:
-        certfile = str(server_pem)
-        # print("certfile: ", certfile)
-        # from IPython import embed;embed()
-
     print(f"Serving HTTPS on {args.address} port {args.port} (https://{args.address}:{args.port}/) ...")
-    server_address = (args.address, args.port)
+    server_address = (args.address, int(args.port))
     httpd = http.server.HTTPServer(server_address,
                                    http.server.SimpleHTTPRequestHandler)
     httpd.socket = ssl.wrap_socket(
         httpd.socket,
-        server_side=True,
-        certfile=certfile, # path
-        ssl_version=ssl.PROTOCOL_TLSv1)
+        keyfile=args.keyfile,
+        certfile=args.certfile,  # path
+        server_side=True)
+
     httpd.serve_forever()
